447963f82cc0e5f0f8a64a9d702d01870d8efb5b
Gelb <GelbJ@INRS-UCS.UQuebec.Ca>
2019-02-07 13:24:12 -0500

    Georaster main objects are now functional !

--- GeoRasters.py
+++ GeoRasters.py
@@ -10,6 +10,8 @@ import shapely
 import numpy as np
 import GeomTools as GT
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection, LineCollection
 from collections import defaultdict
 
 from libs.QuadTree import Index as QdIndex
@@ -71,82 +73,167 @@ class SpArray(np.ndarray):
         Row = int((Coords[1]-self.Extent[1])/self.PixelShape[1])
         return self[Row,Col]
     
-    def Plot(self,Color="viridis") : 
-        plt.imshow(self,extent = (self.Extent[0],self.Extent[2],self.Extent[1],self.Extent[3]),cmap=Color)
-
+    def Plot(self,Color="viridis",Vlim=None) :
+        if Vlim is None : 
+            plt.imshow(self,extent = (self.Extent[0],self.Extent[2],self.Extent[1],self.Extent[3]),cmap=Color)
+        else : 
+            plt.imshow(self,extent = (self.Extent[0],self.Extent[2],self.Extent[1],self.Extent[3]),cmap=Color,vmin=Vlim[0],vmax=Vlim[1])
 
 
 ##############################################################
 ## Classe centrale MultiSpArray
-#Permet l'ecriture de gros raster spatiaux compose de tuiles de sparray
+#Permet la lecture et l'ecriture de gros raster spatiaux compose de tuiles de sparray
 ##############################################################  
 class MultiSpArray(object) : 
     
-    def __init__(self,Extent,MaxSize,Shape,PixelShape,Src=None,Source=None) : 
+    def __init__(self,MaxSize,Source=None,Extent=None,PixelShape=None,Crs=None,Default=None) : 
         """
+        Pour creer un Multi Array, deux option possibles : 
+            -Definir une source, et alors toutes les meta donnees proviennent de cette source
+            -Definir les meta donnees a la main ainsi que la valeur de defaut
+        
         Extent : une etendue specifie ainsi : (minX,minY,MaxX,MaxY)
-        src : un wkt de projection tel que renvoye par osr
-        NB : Shape=(Row,Col)
+        Crs : un wkt de projection tel que renvoye par osr
+        NB : Shape=(Width,Height)
         NB : PixelShape = (Width,Height)
+        NB : TileIndex[Colonne,Row] = {'Extent': [MinX, MinY, MaxX, MaxY], 'Pxs': [StartCol, EndCol, StartRow, EndRow]}
+        NB : Shape2=(Cols,Rows)
         """
         #1) setting des parametres
-        self.Extent = Extent
-        self.Shape = Shape
-        self.PixelShape = PixelShape
+        if Source is None :
+            self.Extent = Extent
+            self.Shape = [Extent[2]-Extent[0],Extent[3]-Extent[1]]
+            self.PixelShape = PixelShape
+            self.Crs=Crs
+            self.Default=Default
+        else : 
+            DataSource = gdal.Open(Source)
+            self.Crs = DataSource.GetProjection()
+            ulx, xres, xskew, uly, yskew, yres  = DataSource.GetGeoTransform()
+            lrx = ulx + (DataSource.RasterXSize * xres)
+            lry = uly + (DataSource.RasterYSize * yres)
+            self.Extent = [ulx,lry,lrx,uly]
+            self.Shape = [self.Extent[2]-self.Extent[0],self.Extent[3]-self.Extent[1]]
+            self.PixelShape = [xres,-yres]
+            self.Shape2 = [DataSource.RasterXSize,DataSource.RasterYSize]
+        
         self.Source = Source
-        self.Src=Src
         self.MaxSize=MaxSize
+        self.__Arrays = defaultdict(lambda : None)
         #2) construire la structures des tuiles
-        NbWidth = int(self.Shape[1]/MaxSize)
-        if NbWidth*MaxSize<self.Shape[1] : 
+        NbWidth = int(self.Shape[0]/MaxSize)
+        if NbWidth*MaxSize<self.Shape[0] : 
             NbWidth+=1
-        NbHeight = int(Shape[0]/MaxSize)
-        if NbHeight*MaxSize<self.Shape[0] : 
+        NbHeight = int(self.Shape[1]/MaxSize)
+        if NbHeight*MaxSize<self.Shape[1] : 
             NbHeight+=1
         TotCol=0
         #iterer sur les colonnes pour delimiter les tuiles
         self.TileIndex = {}
         for i in range(NbWidth) : 
-            if TotCol+MaxSize<=self.Shape[1] : 
+            if TotCol+MaxSize<=self.Shape[0] : 
                 AddCol = MaxSize
             else : 
-                AddCol = self.Shape[1]-TotCol
+                AddCol = self.Shape[0]-TotCol
             StartCol = TotCol
             EndCol = TotCol + AddCol
-            MinX = self.Extent[0]+self+PixelShape[0]*StartCol
-            MaxX = self.Extent[0]+self+PixelShape[0]*EndCol
+            MinX = self.Extent[0]+self.PixelShape[0]*StartCol
+            MaxX = self.Extent[0]+self.PixelShape[0]*EndCol
             #iterer sur les lignes pour delimiter les tuiles
             TotRow = 0
             for j in range(NbHeight) : 
-                if TotRow+MaxSize<=self.Shape[0] : 
+                if TotRow+MaxSize<=self.Shape[1] : 
                     AddRow = MaxSize
                 else : 
-                    AddRow = self.Shape[0]-TotRow
+                    AddRow = self.Shape[1]-TotRow
                 StartRow = TotRow
                 EndRow = TotRow+AddRow
-                MinY = self.Extent[3]-self.PixelShape[1]*StartRow
-                MaxY = self.Extent[3]-self.PixelShape[1]*EndRow
-                self.TileIndex[(i,j)] = {"Extent":[MinX,MinY,MaxX,MaxY],"Pxs":[StartRow,EndRow,StartCol,EndCol]}
+                MaxY = self.Extent[3]-self.PixelShape[1]*StartRow
+                MinY = self.Extent[3]-self.PixelShape[1]*EndRow
+                self.TileIndex[(i,j)] = {"Extent":[MinX,MinY,MaxX,MaxY],"Pxs":[int(StartCol),int(EndCol),int(StartRow),int(EndRow)]}
                 TotRow+=AddRow
             TotCol+=AddCol
         #Creer finalement l'index spatial
         #BBox demande par QIndex : (xmin,ymin,xmax,ymax)
         self.SpIndex = QdIndex(self.Extent)
-        for key,value in self.TileIndex.values() : 
+        for key,value in self.TileIndex.items() : 
+            print(key)
             self.SpIndex.insert(key,value["Extent"])
+            
+    def GetRaster(self,Key,FromSource=True) : 
+        """
+        Permet d'acceder aux donnees presentes dans le raster source
+        """
+        if self.__Arrays[Key] is None or FromSource :
+            Coordinates = self.TileIndex[Key]
+            Window = Coordinates["Pxs"]
+            Extent = Coordinates["Extent"]
+            DataSource = gdal.Open(self.Source)
+            print("key : "+str(Key))
+            print("Window : "+str(Window))
+            print("xoff="+str(Window[0]))
+            print("yoff="+str(Window[2]))
+            print("xsize="+str(Window[1]- Window[0]))
+            print("ysize="+str(Window[3]-Window[2]))
+            Data = DataSource.ReadAsArray(xoff=Window[0],yoff=Window[2], xsize = Window[1]- Window[0], ysize = Window[3]-Window[2])
+            print(Data)
+            Array = SpArray(Data,self.Crs,Extent)
+            del DataSource
+        else : 
+            Array = self.__Arrays[Key]
+        return Array
+    
+    
+    def PlotGrid(self,LineColor="g",LineWidth=2) : 
+        """
+        Permet de dessiner les emprises des tuiles du raster multiple
+        """
+        Lines = []
+        for Key,Value in self.TileIndex.items() : 
+            Extent = Value["Extent"]
+            Lines.append([(Extent[0],Extent[1]),(Extent[1],Extent[1]),(Extent[1],Extent[3]),(Extent[0],Extent[3]),(Extent[0],Extent[1])])
+        lc = LineCollection(Lines,colors=LineColor,linewidths=LineWidth)
+        fig = plt.gcf()
+        ax = fig.axes[0]
+        ax.add_collection(lc)
+        ax.set_xlim(self.Extent[0],self.Extent[2])
+        ax.set_ylim(self.Extent[1],self.Extent[3])
+        plt.show()
+            
+            
+            
         
+    def Plot(self,Extent=None,FromSource=True) :
+        """
+        Permet de dessiner tout ou partie du raster
+        """
+        if Extent is None : 
+            Extent = self.Extent
         
-        
-
+        Keys = self.SpIndex.intersect(Extent)
+        f = plt.figure()
+        Arrays=[]
+        Mins=[]
+        Maxs=[]
+        for Key in Keys : 
+            Raster = self.GetRaster(Key,FromSource)
+            Arrays.append(Raster)
+            Mins.append(np.min(Raster))
+            Maxs.append(np.max(Raster))
+        for Array in Arrays : 
+            Array.Plot(Vlim=[np.min(Mins),np.max(Maxs)])
+        a = f.axes[0]
+        a.set_xlim(Extent[0],Extent[2])
+        a.set_ylim(Extent[1],Extent[3])
+        plt.show()
+        plt.colorbar()
    
 ################################
 #Tests !
         
 if __name__=="__main__" : 
-    Values = [[0,1,2,3],[4,5,6,7]]
-    Extent = [0,0,20,20]
-    Src = "blabla"
-    Raster = SpArray(Values,Src,Extent)
-    print(Raster.Sample([1,1]))
-    print(Raster.Sample([19,19]))
-    Raster.Plot("magma")
+    Link = r"L:\TEMP\BatiParis_TestRaster.tif"
+    Raster1 = MultiSpArray(100,Source=Link)
+    Raster1.Plot()
+    Raster1.PlotGrid(LineColor="r")
+    
