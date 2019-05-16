1bb8193064b3a83d5b5b51de2aa49230efedd8f8
jejeplusfaim <gelbjeremy22@gmail.com>
2019-05-16 18:33:27 -0400

    A little update of the PathLib, exploring the pattern matching for walkfiles

--- PathLib.py
+++ PathLib.py
@@ -6,7 +6,8 @@ Created on Tue May 14 09:48:50 2019
 """
 
 import os
-
+import fnmatch
+import re
 
 
 
@@ -37,13 +38,32 @@ class Path(str) :
     def isdir(self) : 
         return os.path.isdir(self)
     
-    def walkfiles(self) : 
+    def walkfiles(self,include=None,exclude=None) : 
+        # transform glob patterns to regular expressions
+        if type(include) is str : 
+            include = [include]
+        if type(exclude) is str : 
+            exclude = [exclude]
+        if include is not None :
+            includes = r'|'.join([fnmatch.translate(x) for x in include])
+        if exclude is not None :
+            excludes = r'|'.join([fnmatch.translate(x) for x in exclude]) or r'$.'
         if self.isdir() == False : 
             raise ValueError("I am not a folder : "+self)
         else :
             for root, dirs, files in os.walk(self, topdown=False) : 
                 for file in files : 
-                    yield Path(root).joinpath(file)
+                    if include is None and exclude is None : 
+                        yield Path(root).joinpath(file)
+                    elif include is not None and exclude is None : 
+                        if re.match(includes,file) :
+                            yield Path(root).joinpath(file)
+                    elif include is None and exclude is not None : 
+                        if not re.match(excludes,file) : 
+                            yield Path(root).joinpath(file)
+                    else : 
+                        if re.match(includes,file) and not re.match(excludes,file) : 
+                            yield Path(root).joinpath(file)
     
     def walkdirs(self) : 
         if self.isdir() == False : 
@@ -55,3 +75,6 @@ class Path(str) :
         
         
     
+titi = Path('G:/___Data These')
+E = titi.walkfiles("*.shp","*ID1_PA*")
+print(next(E))
